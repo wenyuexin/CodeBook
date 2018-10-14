c3eff0d272ae2157f278acc73d7ccafd81d5ed21
Apollo4634 <wenyuexin@163.com>
2018-10-14 19:53:39 +0800

    10141953

--- src/javabasic/collection/CapacityOfArrayList.java
+++ src/javabasic/collection/CapacityOfArrayList.java
@@ -4,15 +4,15 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 /*
- * ArrayList并没有可以直接获取capacity信息的成员
- * 这里使用反射获取ArrayList的capacity值
+ * ArrayList锟斤拷没锟叫匡拷锟斤拷直锟接伙拷取capacity锟斤拷息锟侥筹拷员
+ * 锟斤拷锟斤拷使锟矫凤拷锟斤拷锟饺rrayList锟斤拷capacity值
  */
 public class CapacityOfArrayList {
 	public static void main(String[] args) {
 		ArrayList<Integer> arrayList = new ArrayList<>();
         System.out.println(getArrayListCapacity(arrayList));
  
-        //增加元素，使其扩容
+        //锟斤拷锟斤拷元锟截ｏ拷使锟斤拷锟斤拷锟斤拷
         arrayList.add(0);
         System.out.println(getArrayListCapacity(arrayList));
  
@@ -26,7 +26,8 @@ public class CapacityOfArrayList {
     }
  
     public static int getArrayListCapacity(ArrayList<?> arrayList) {
-        Class<ArrayList> arrayListClass = ArrayList.class;
+        @SuppressWarnings("rawtypes")
+		Class<ArrayList> arrayListClass = ArrayList.class;
         try {
             Field field = arrayListClass.getDeclaredField("elementData");
             field.setAccessible(true);
