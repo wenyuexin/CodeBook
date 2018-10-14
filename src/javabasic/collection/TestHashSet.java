bb0efe41f8474ea5ee6bfb418869ae42f9f0bc29
Apollo4634 <apollo4634@gmail.com>
2018-10-14 23:22:44 +0800

    10142322

---
+++ src/javabasic/collection/TestHashSet.java
@@ -0,0 +1,18 @@
+package javabasic.collection;
+
+/**
+ * 测试
+ * @author Administrator
+ *
+ */
+public class TestHashSet {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class MyHashSet {
+	public void test1() {
+		;
+	}
+}
