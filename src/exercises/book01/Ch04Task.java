8cc4a3f047240c6154dd8bacc44210da3773482e
Apollo4634 <wenyuexin@163.com>
2018-10-14 18:11:14 +0800

    10141811

---
+++ src/exercises/book01/Ch04Task.java
@@ -0,0 +1,20 @@
+package exercises.book01;
+
+public class Ch04Task {
+	public static void main(String[] args) {
+		//Task1
+		Ch04Task01 t1 = new Ch04Task01();
+		System.out.println(t1.isOdd(19));
+		System.out.println(t1.isOdd(20));
+	}
+}
+
+/**
+ * T1: 判断变量的奇偶
+ */
+class Ch04Task01 {
+		
+	boolean isOdd(int num) {
+		return 1==num%2;
+	}
+}
