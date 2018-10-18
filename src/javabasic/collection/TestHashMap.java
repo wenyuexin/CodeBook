0f62bc0fd309142c5c7bebff2228bf62451c4791
Apollo4634 <wenyuexin@163.com>
2018-10-18 22:03:21 +0800

    10182203

---
+++ src/javabasic/collection/TestHashMap.java
@@ -0,0 +1,23 @@
+package javabasic.collection;
+
+import java.util.HashMap;
+
+public class TestHashMap {
+	public static void main(String[] args) {
+		MyHashMap hm = new MyHashMap();
+		hm.test1();
+		
+	}
+}
+
+class MyHashMap {
+	public void test1() {
+		HashMap<Integer, String> hashMap = new HashMap<>();
+		hashMap.put(Integer.valueOf(10),"aab");
+		hashMap.put(Integer.valueOf(20),"bbc");
+		hashMap.put(Integer.valueOf(40),"ccd");
+		hashMap.put(Integer.valueOf(30),null);
+		System.out.println(hashMap);
+		
+	}
+}
