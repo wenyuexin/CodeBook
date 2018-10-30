0f46e54c8a99a040e7dbdfafc69c7b97a612af3e
Apollo4634 <wenyuexin@163.com>
2018-10-30 22:04:10 +0800

    10302204

--- src/javabasic/reflection/TestMethod.java
+++ src/javabasic/reflection/TestMethod.java
@@ -11,15 +11,29 @@ public class TestMethod {
 		for(Method m: stuMethods) { 
 			System.out.println(m);
 		}
-		
 		System.out.println();
+		
 		Method[] stuDeclaredMethods = stuClass.getDeclaredMethods();//本类定义的方法
 		for(Method m: stuDeclaredMethods) {
 			System.out.println(m);
 		}
+		System.out.println();
 		
 		System.out.println("===== 2 =====");
+		//获取定义该方法的类的Class
+		for(Method m: stuMethods) {
+			System.out.println(m.getDeclaringClass());
+		}
 		System.out.println();
+		
+		//获取该方法的返回值的Class类
+		for(Method m: stuMethods) {
+			System.out.print(m.getReturnType()+"\t --\t");
+			System.out.println(m.getGenericReturnType());
+		}
+		
+
+		
 		
 		
 	}
