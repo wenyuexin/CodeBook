f2ab3ff15122f411abb6d039f2e0b0c38d30d565
Apollo4634 <apollo4634@gmail.com>
2018-11-02 21:54:26 +0800

    11022154

---
+++ src/exercises/book01/Ch16Task.java
@@ -0,0 +1,19 @@
+package exercises.book01;
+
+public class Ch16Task {
+
+	void changeArr() {
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = new int[]{1,3,5,7};
+		System.out.println(arr[0]);
+		
+		Class<?> clazz = arr.getClass();
+		System.out.println(clazz);
+		
+		System.out.println(arr.length);
+		
+	}
+}
