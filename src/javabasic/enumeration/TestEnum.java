0881b36338f9afbac64bfe933399230e233b4871
Apollo4634 <wenyuexin@163.com>
2018-11-06 17:43:46 +0800

    11061743

--- src/javabasic/enumeration/TestEnum.java
+++ src/javabasic/enumeration/TestEnum.java
@@ -1,30 +1,51 @@
 package javabasic.enumeration;
 
+import java.math.RoundingMode;
+
 /** 
-* @author Apollo4634 
-* @creation 2018/11/06
-*/
+ * @author Apollo4634 
+ * @creation 2018/11/06
+ */
 public class TestEnum {
 	enum Constants {
-		c1,
-		c2,
-		c3
+		C1,
+		C2,
+		C3
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("===== 1 =====");
-		System.out.println(Constants.c1);
-		System.out.println(Constants.c2.ordinal());
-		
-		Constants constants = Constants.valueOf("c3");
-		System.out.println(constants.ordinal());
+		System.out.println(Constants.C1);
+		System.out.println(Constants.C2.ordinal()); //返回枚举量的序数
+		System.out.println(Constants.C3 instanceof Constants);
 		
 		System.out.println("===== 2 =====");
-		Constants[] arr = Constants.values();
+		Constants constants = Constants.valueOf("C3"); //返回对应字符串的枚举实例
+		int num = constants.ordinal();
+		String name = constants.name(); //获取枚举量对应的名字
+		System.out.println(name+" -- "+num);
+		System.out.println();
+		
+		System.out.println("===== 3 =====");
+		Constants[] arr = Constants.values(); //返回包含所有枚举量的数组
 		for(Constants c: arr) {
 			System.out.println(c);
 		}
 		
+		System.out.println("===== 4 =====");
+		Class<?> cl = constants.getDeclaringClass();
+		System.out.println(cl);
 		
+		System.out.println("===== 5 =====");
+		RoundingMode rm = RoundingMode.valueOf(0);//RoundingMode自定义的方法
+		System.out.println(rm);
+		
+		RoundingMode rm2 = RoundingMode.valueOf("DOWN"); //来自Enum的方法
+		System.out.println(rm2);
+		
+		System.out.println("===== 6 =====");
+		System.out.println(rm.compareTo(rm2));
+		System.out.println(rm.compareTo(rm));
+		System.out.println(rm2.compareTo(rm));
 	}
 }
---
+++ src/javabasic/enumeration/TestMyEnum.java
@@ -0,0 +1,45 @@
+package javabasic.enumeration;
+
+/**
+ * @author Apollo4634
+ * @creation 2018/11/06
+ */
+public class TestMyEnum {
+	
+	enum MyEnum {
+		C1,
+		C2,
+		C3;
+	}
+	
+	enum MyEnum2 {
+		C1(2),
+		C2(0),
+		C3(1);
+		
+		final int e;
+		
+		MyEnum2(int e) {
+			this.e = e;
+		}
+	}
+	
+	public static void main(String[] args) {
+		/**
+		 * 枚举类的实现原理：
+		 * 使用关键字enum创建枚举类型并编译后，
+		 * 编译器会为我们生成一个相关的类，
+		 * 这个类继承了Java API中的java.lang.Enum类
+		 * 
+		 * 枚举类的参考：
+		 * java.base模块中的java.math定义了：
+		 * public enum RoundingMode
+		 */
+		
+		System.out.println("===== 1 =====");
+		System.out.println(MyEnum.C1+" "+MyEnum.C1.ordinal());
+		System.out.println(MyEnum.C1);
+		
+		
+	}
+}
