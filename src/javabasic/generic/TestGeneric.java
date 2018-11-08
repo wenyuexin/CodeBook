d5c947bafcd17e22e54238e9aceae82dc207886c
Apollo4634 <wenyuexin@163.com>
2018-11-08 20:39:18 +0800

    11082039

---
+++ src/javabasic/generic/TestGeneric.java
@@ -0,0 +1,13 @@
+package javabasic.generic;
+
+/**
+ * @author Apollo4634
+ * @creation 2018/11/08
+ */
+public class TestGeneric {
+
+	public static void main(String[] args) {
+		System.out.println("===== 1 =====");
+		
+	}
+}
