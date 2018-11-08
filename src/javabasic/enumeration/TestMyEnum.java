ba4b4ce5fb12ec56675ae1c0256f54c5b157c861
Apollo4634 <wenyuexin@163.com>
2018-11-08 16:49:07 +0800

    11081649

--- src/javabasic/enumeration/TestMyEnum.java
+++ src/javabasic/enumeration/TestMyEnum.java
@@ -1,13 +1,15 @@
 package javabasic.enumeration;
 
+
+
 /**
  * @author Apollo4634
  * @creation 2018/11/06
  */
 public class TestMyEnum {
-	
+	//这里的MyEnum相当于TestMyEnum的内部类
 	enum MyEnum {
-		C1,
+		C1, //枚举量相当于枚举类内部的几个枚举实例
 		C2,
 		C3;
 	}
@@ -25,24 +27,59 @@ public class TestMyEnum {
 	}
 	
 	enum MyEnum3 {
-		C1{
+		C1(3){
 			@Override
 			void print() {
-				System.out.println("print C1");
+				System.out.println("print C1 - "+e);
+			}
+			
+			@SuppressWarnings("unused")
+			void print2() {
+				System.out.println("print2 C1");
 			}
 		},
-		C2,
-		C3;
+		C2(4){
+			@Override
+			void print() {
+				System.out.println("print C2 - "+e);
+			}
+		},
+		C3(5){
+			@Override
+			void print() {
+				System.out.println("print C3 - "+e);
+			}
+		};
+
+		private MyEnum3() {}
 		
+		private MyEnum3(int n) { this.e = n; }
+		
+		//@SuppressWarnings("unused")
+		int e;
+		//private static int e = 0;
+		
+		//枚举类类似于抽象类，可以定义一些方法
 		abstract void print();
+	}
+	
+	enum MyEnum4 {
+		C1(7), C2(8), C3(9);
+
+		//@SuppressWarnings("unused")
+		//int e;
+		private int num = 0;
+				
+		private MyEnum4() {}
 		
-		final int e;
-		
-		MyEnum3() {
+		private MyEnum4(int n) { this.num = n; }
+
+		public int getNum() {
+			return num;
 		}
-		
-		MyEnum3(int e) {
-			this.e = e;
+
+		public void setNum(int num) {
+			this.num = num;
 		}
 	}
 	
@@ -60,8 +97,17 @@ public class TestMyEnum {
 		
 		System.out.println("===== 1 =====");
 		System.out.println(MyEnum.C1+" "+MyEnum.C1.ordinal());
-		System.out.println(MyEnum.C1);
 		
+		MyEnum3.C1.print();
+		MyEnum3.C3.print();
+		MyEnum3.C1.print();
 		
+		//MyEnum3.C1.print2();
+		System.out.println(MyEnum3.C1.e);
+		System.out.println(MyEnum4.C2.getNum());
+		
+		//不能实例化枚举对象
+		//TestMyEnum testMyEnum = new TestMyEnum();
+		//MyEnum3 me = new TestMyEnum.MyEnum3(6);
 	}
 }
