7372986bc927af22552203d9d717891b4e92eef0
Apollo4634 <apollo4634@gmail.com>
2018-11-10 23:37:40 +0800

    11102337

--- src/javabasic/generic/TestGeneric.java
+++ src/javabasic/generic/TestGeneric.java
@@ -8,7 +8,27 @@ public class TestGeneric {
 
 	public static void main(String[] args) {
 		System.out.println("===== 1 =====");
-		
-		
+		MyClass<Integer> mc = new MyClass<Integer>();
+		Integer num = Integer.valueOf(12);
+		mc.setObj(num);
+		mc.print();
 	}
 }
+
+class MyClass<T> {
+	T obj;
+	
+	public T getObj() {
+		return obj;
+	}
+
+	public void setObj(T obj) {
+		this.obj = obj;
+	}
+
+	void print() {
+		System.out.println(obj);
+	}
+}
+
+
