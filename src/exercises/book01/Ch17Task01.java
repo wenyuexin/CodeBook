12295c842be8cd1869030f96b632cf80076d18d1
Apollo4634 <apollo4634@gmail.com>
2018-11-16 00:15:33 +0800

    11160015

--- src/exercises/book01/Ch17Task01.java
+++ src/exercises/book01/Ch17Task01.java
@@ -5,28 +5,32 @@ package exercises.book01;
  * @creation 2018/11/15
  */
 public class Ch17Task01 {
+	
+	/**
+	 * 定义一个枚举类类型，使用switch语句获取枚举类型的值
+	 */
 	public static void main(String[] args) {
 		
 		MyEnum enum1 = MyEnum.E3;
 		
 		switch (enum1.ordinal()) {
 		case 0:
-			System.out.println("a");
+			System.out.println(0);
 			break;
 		case 1:
-			System.out.println("b");
+			System.out.println(1);
 			break;
 		case 2:
-			System.out.println("c");
+			System.out.println(2);
 			break;
 		case 3:
-			System.out.println("d");
+			System.out.println(3);
 			break;
 		case 4:
-			System.out.println("e");
+			System.out.println(4);
 			break;
 		case 5:
-			System.out.println("f");
+			System.out.println(5);
 			break;
 		default:
 			break;
