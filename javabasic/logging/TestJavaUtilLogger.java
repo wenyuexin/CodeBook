ac1ba5a0ea43216652355c87200e604db6069415
Apollo4634 <wenyuexin@163.com>
2019-01-01 14:22:27 +0800

    01011422

---
+++ javabasic/logging/TestJavaUtilLogger.java
@@ -0,0 +1,34 @@
+package logging;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/** 
+ * @author Apollo4634 
+ * @creation 2018/12/31 16:12
+ * 
+ * java日志系统
+ */
+
+public class TestJavaUtilLogger {
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger("logging");
+		logger.setLevel(Level.ALL);
+		try {
+			Handler handler = new FileHandler("logging_TestJavaUtilLogger.log");
+			logger.addHandler(handler);
+			
+			logger.throwing("TestJavaUtilLogger", "main", null);
+		} catch (SecurityException e) {
+			logger.log(Level.INFO, "error for test", e);
+			e.printStackTrace();
+		} catch (IOException e) {
+			logger.log(Level.INFO, "error2 for test", e);
+			e.printStackTrace();
+		}
+		
+	}
+}
