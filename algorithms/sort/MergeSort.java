7c1b215bdeb3b6c05cbd64a3cf6a4230b50bff9b
Apollo4634 <apollo4634@gmail.com>
2019-02-05 21:42:05 +0800

    02052142

--- algorithms/sort/MergeSort.java
+++ algorithms/sort/MergeSort.java
@@ -24,7 +24,7 @@ public class MergeSort {
 		}
 	}
 	
-	public static <T extends Comparable<? super T>>
+	private static <T extends Comparable<? super T>>
 	void subsort(T[] arr, T[] aux, int left, int right) {
 		if (left<right) {
 			int mid = (left+right)/2;
@@ -42,17 +42,89 @@ public class MergeSort {
 	}
 	
 	
-	//
-	
-	
-	
-	public static void main(String[] args) {
-		Integer[] arr = new Integer[] {
-			-59, -29, -49, -86, -15, 90, 31, -37, -45, -35,
-			-89, 74, 76, 11, 89, -30, -58, -15, 6, -80};
+	//int
+	private static void merge(int[] arr, int[] aux, int left, int mid, int right) {
+		for (int i = left; i <= right; i++) aux[i] = arr[i];
 		
-		System.out.println(Arrays.toString(arr));
-		sort(arr);
-		System.out.println(Arrays.toString(arr));
+		int i=left, j=mid+1, k=left;
+		while (i<=mid || j<=right) {
+			if (i>mid) arr[k++] = aux[j++];
+			else if (j>right) arr[k++] = aux[i++];
+			else if (SortUtils.less(aux[j], aux[i])) arr[k++] = aux[j++];
+			else arr[k++] = aux[i++];
+		}
+	}
+	
+	private static void subsort(int[] arr, int[] aux, int left, int right) {
+		if (left<right) {
+			int mid = (left+right)/2;
+			subsort(arr, aux, left, mid);
+			subsort(arr, aux, mid+1, right);
+			merge(arr, aux, left, mid, right);
+		}
+	}
+	
+	public static void sort(int[] arr) {
+		int arrLen = arr.length;
+		int[] aux = Arrays.copyOf(arr, arrLen);
+		subsort(arr, aux, 0, arrLen-1);
+	}
+	
+	
+	//long
+	private static void merge(long[] arr, long[] aux, int left, int mid, int right) {
+		for (int i = left; i <= right; i++) aux[i] = arr[i];
+		
+		int i=left, j=mid+1, k=left;
+		while (i<=mid || j<=right) {
+			if (i>mid) arr[k++] = aux[j++];
+			else if (j>right) arr[k++] = aux[i++];
+			else if (SortUtils.less(aux[j], aux[i])) arr[k++] = aux[j++];
+			else arr[k++] = aux[i++];
+		}
+	}
+	
+	private static void subsort(long[] arr, long[] aux, int left, int right) {
+		if (left<right) {
+			int mid = (left+right)/2;
+			subsort(arr, aux, left, mid);
+			subsort(arr, aux, mid+1, right);
+			merge(arr, aux, left, mid, right);
+		}
+	}
+	
+	public static void sort(long[] arr) {
+		int arrLen = arr.length;
+		long[] aux = Arrays.copyOf(arr, arrLen);
+		subsort(arr, aux, 0, arrLen-1);
+	}
+	
+	
+	//double
+	private static void merge(double[] arr, double[] aux, int left, int mid, int right) {
+		for (int i = left; i <= right; i++) aux[i] = arr[i];
+		
+		int i=left, j=mid+1, k=left;
+		while (i<=mid || j<=right) {
+			if (i>mid) arr[k++] = aux[j++];
+			else if (j>right) arr[k++] = aux[i++];
+			else if (SortUtils.less(aux[j], aux[i])) arr[k++] = aux[j++];
+			else arr[k++] = aux[i++];
+		}
+	}
+	
+	private static void subsort(double[] arr, double[] aux, int left, int right) {
+		if (left<right) {
+			int mid = (left+right)/2;
+			subsort(arr, aux, left, mid);
+			subsort(arr, aux, mid+1, right);
+			merge(arr, aux, left, mid, right);
+		}
+	}
+	
+	public static void sort(double[] arr) {
+		int arrLen = arr.length;
+		double[] aux = Arrays.copyOf(arr, arrLen);
+		subsort(arr, aux, 0, arrLen-1);
 	}
 }
