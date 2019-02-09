c7a6ac15ade8eb1db84eceef78eeec070da7c567
Apollo4634 <wenyuexin@163.com>
2019-02-09 22:45:30 +0800

    02092245

--- algorithms/sort/BubbleSort.java
+++ algorithms/sort/BubbleSort.java
@@ -30,6 +30,16 @@ public class BubbleSort {
 		}
 	}
 	
+	public static void sort(long[] arr) {
+		int arrLen = arr.length;
+		for (int i = arrLen-1; i > 0; i--) {
+			for(int j=0; j<i; j++) {
+				if(SortUtils.less(arr[j+1], arr[j])) 
+					SortUtils.swap(arr, j, j+1);
+			}
+		}
+	}
+	
 	public static void sort(double[] arr) {
 		int arrLen = arr.length;
 		for (int i = arrLen-1; i > 0; i--) {
