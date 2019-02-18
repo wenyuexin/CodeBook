5ce395b794304c26c4439b50b47938ba0fdd7ea6
Apollo4634 <apollo4634@gmail.com>
2019-02-18 22:56:02 +0800

    02182255

--- algorithms/sort/HeapSort.java
+++ algorithms/sort/HeapSort.java
@@ -16,7 +16,7 @@ public class HeapSort {
 	void sink(T[] arr, int idx, int N) {
 		System.out.println(Arrays.toString(arr));
 		
-		for (int i = idx+1; 2*i <= N; i *= 2) {
+		for (int i = idx+1; 2*i < N; i *= 2) {
 			int j = SortUtils.less(arr[2*i-1],arr[2*i])? 2*i : 2*i-1;
 			if (!SortUtils.less(arr[i-1],arr[j])) break;
 			else SortUtils.swap(arr, i-1, j);
