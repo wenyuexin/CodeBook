4dfa289159125bf79b2537cbd7bd5b4cfa089643
Apollo4634 <wenyuexin@163.com>
2019-03-26 00:36:02 +0800

    0326-0035 digraph

---
+++ algorithms/EdgeWeightedDiagraph.java
@@ -0,0 +1,12 @@
+
+/** 
+ * @author Apollo4634 
+ * @create 2019/03/26
+ */
+
+public class EdgeWeightedDiagraph {
+
+	EdgeWeightedDiagraph(int[][] prerequisites, double[] weight) {
+		
+	}
+}
---
+++ algorithms/diagraph/Dijkstra.java
@@ -0,0 +1,10 @@
+package diagraph;
+
+/** 
+ * @author Apollo4634 
+ * @create 2019/03/26
+ */
+
+public class Dijkstra {
+	
+}
