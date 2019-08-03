4509da2efb10f7dbceb92b8a8844d2328c63b40e
Apollo4634 <apollo4634@gmail.com>
2019-08-03 20:50:26 +0800

    0803-2050 Executor

---
+++ concurrent/notes/Executor框架.md
@@ -0,0 +1,17 @@
+# Executor框架
+
+以interface java.concurrent.Executor为基础的Executor框架是Java提供的处理并发的框架。
+
+```
+Executor框架
+
+Executor
+    ├── ExecutorService
+    |   ├── 
+    |   └── ...
+    └── ScheduledExecutorService
+        ├── 
+            └── ...
+        
+```
+
---
+++ concurrent/src/ExecutorTest.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.*;
+
+/**
+ * @author Apollo4634
+ * @create 2019/08/03
+ */
+
+public class ExecutorTest {
+    private static final ExecutorService exec = Executors.newFixedThreadPool(2);
+
+    public static void main(String[] args) {
+        Runnable task1 = ()-> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("A "+i);
+            }
+        };
+
+        Callable<Integer> task2 = ()-> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("B "+i);
+            }
+            return 100;
+        };
+
+        Future<?> ret1 = exec.submit(task1);
+        Future<Integer> ret2 = exec.submit(task2);
+
+        try {
+            System.out.println(ret2.get());
+        } catch (ExecutionException e) {
+            System.out.println(e.toString());
+        } catch (InterruptedException e) {
+            System.out.println(e.getCause().toString());
+        }
+
+        exec.shutdown();//每次使用完后，都要关闭线程池
+    }
+}
