df707e58536d027910a5949a9ab595e9941479e6
Apollo4634 <apollo4634@gmail.com>
2019-08-08 14:05:10 +0800

    0808-1405

--- algorithms/src/sort/BubbleSort.java
+++ algorithms/src/sort/BubbleSort.java
@@ -11,8 +11,7 @@ public class BubbleSort {
 	
 	public static <T extends Comparable<? super T>> 
 	void sort(T[] arr) {
-		int arrLen = arr.length;
-		for (int i = arrLen-1; i > 0; i--) {
+		for (int i = arr.length-1; i > 0; i--) {
 			for(int j=0; j<i; j++) {
 				if(SortUtils.less(arr[j+1], arr[j])) 
 					SortUtils.swap(arr, j, j+1);
@@ -21,8 +20,7 @@ public class BubbleSort {
 	}
 	
 	public static void sort(int[] arr) {
-		int arrLen = arr.length;
-		for (int i = arrLen-1; i > 0; i--) {
+		for (int i = arr.length-1; i > 0; i--) {
 			for(int j=0; j<i; j++) {
 				if(SortUtils.less(arr[j+1], arr[j])) 
 					SortUtils.swap(arr, j, j+1);
@@ -31,8 +29,7 @@ public class BubbleSort {
 	}
 	
 	public static void sort(long[] arr) {
-		int arrLen = arr.length;
-		for (int i = arrLen-1; i > 0; i--) {
+		for (int i = arr.length-1; i > 0; i--) {
 			for(int j=0; j<i; j++) {
 				if(SortUtils.less(arr[j+1], arr[j])) 
 					SortUtils.swap(arr, j, j+1);
@@ -41,8 +38,7 @@ public class BubbleSort {
 	}
 	
 	public static void sort(double[] arr) {
-		int arrLen = arr.length;
-		for (int i = arrLen-1; i > 0; i--) {
+		for (int i = arr.length-1; i > 0; i--) {
 			for(int j=0; j<i; j++) {
 				if(SortUtils.less(arr[j+1], arr[j])) 
 					SortUtils.swap(arr, j, j+1);
