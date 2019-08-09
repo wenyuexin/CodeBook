27731f16d74a16e264ab9b42c5b04658fb3c9a8f
Apollo4634 <apollo4634@gmail.com>
2019-08-10 00:51:18 +0800

    0810-0045 剑指offer 08

---
+++ exercise/src/book_9787121310928/Problem_08.java
@@ -0,0 +1,19 @@
+package book_9787121310928;
+
+import book_9787121310928.utility.BinaryTreeNode;
+
+/**
+ * 二叉树的下一个节点
+ *
+ * @author Apollo4634
+ * @create 2019/08/09
+ */
+
+public class Problem_08 {
+    static class Solution {
+        BinaryTreeNode findNext(BinaryTreeNode treeRoot, BinaryTreeNode node) {
+
+            return null;
+        }
+    }
+}
