0630d8d03b8d902e1f74890f1068d00f840b9423
Apollo4634 <apollo4634@gmail.com>
2019-08-11 22:17:57 +0800

    0811-2217 剑指offer 08 - solved

--- exercise/src/book_9787121310928/utility/BinaryTreeNode.java
+++ exercise/src/book_9787121310928/utility/BinaryTreeNode.java
@@ -9,14 +9,20 @@ public class BinaryTreeNode {
     public int value;
     public BinaryTreeNode left;
     public BinaryTreeNode right;
+    public BinaryTreeNode parent;
 
     public BinaryTreeNode(int value) {
-        this.value = value;
+        this(value, null, null, null);
     }
 
     public BinaryTreeNode(int value, BinaryTreeNode left, BinaryTreeNode right) {
+        this(value, left, right, null);
+    }
+
+    public BinaryTreeNode(int value, BinaryTreeNode left, BinaryTreeNode right, BinaryTreeNode parent) {
         this.value = value;
         this.left = left;
         this.right = right;
+        this.parent = parent;
     }
 }
