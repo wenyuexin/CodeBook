9ca2c11b5580ab8c7aeb5771de39681b92ff2e1c
Apollo4634 <=>
2019-08-12 17:21:08 +0800

    0812-1720 剑指offer 18 - solved

---
+++ exercise/src/book_9787121310928/Problem_18_1.java
@@ -0,0 +1,20 @@
+package book_9787121310928;
+
+import book_9787121310928.utility.ListNode;
+
+/**
+ * 删除链表的节点
+ * 给定单向链表的头和一个节点，在O(1)时间内删除该节点
+ *
+ * @author Apollo4634
+ * @create 2019/08/12
+ */
+
+public class Problem_18_1 {
+
+    static class Solution {
+        void deleteNode(ListNode listHead, ListNode nodeToBeDelete) {
+
+        }
+    }
+}
