f1341a62c729bf7efb9dd04193aaa7a9d3471c0b
Apollo4634 <apollo4634@gmail.com>
2019-08-14 00:37:09 +0800

    0814-0037 剑指offer 20 - solved

--- exercise/src/book_9787121310928/Problem_20.java
+++ exercise/src/book_9787121310928/Problem_20.java
@@ -10,17 +10,18 @@ package book_9787121310928;
 public class Problem_20 {
 
     static class Solution {
+        private static final int dot = '.' - '0';
         boolean isDigital(String str) {
             if (str == null || str.length() == 0) return false;
-            char[] chars = str.toLowerCase().toCharArray();
-            int eIdx = str.indexOf('e');
-            int ch;
-            if (eIdx >= 0) {
-                String tailingStr = str.substring(eIdx+1);
-                ch = tailingStr.charAt(0);
-                int from = (ch == '+' || ch == '-')? 1 : 0;
-                for (int i = from; i < tailingStr.length(); i++) {
-                    ch = tailingStr.charAt(i) - '0';
+            String lowerStr = str.toLowerCase();
+            int eIdx = lowerStr.indexOf('e');
+            if (eIdx == str.length()-1) return false;
+            char[] chars = lowerStr.toCharArray();
+            if (eIdx >= 0 && eIdx+1 < str.length()) {
+                int ch = chars[eIdx+1];
+                int from = (ch == '+' || ch == '-')? eIdx+2 : eIdx+1;
+                for (int i = from; i < str.length(); i++) {
+                    ch = chars[i] - '0';
                     if (ch < 0 || ch > 9) return false;
                 }
             } else {
@@ -28,13 +29,13 @@ public class Problem_20 {
             }
 
             boolean containsDot = false;
-            ch = str.charAt(0) - '0';
-            int from = (ch == '+' || ch == '-')? 1 : 0;
-            for (int i = 0; i < eIdx; i++) {
-                if (!containsDot && ch == ('.'-'0')) { containsDot = true; continue; }
+            int from = (chars[0] == '+' || chars[0] == '-')? 1 : 0;
+            for (int i = from; i < eIdx; i++) {
+                int ch = chars[i] - '0';
+                if (!containsDot && ch == dot) { containsDot = true; continue; }
                 if (ch < 0 || ch > 9) return false;
             }
-            return false;
+            return true;
         }
     }
 
@@ -49,9 +50,12 @@ public class Problem_20 {
         System.out.println(new Solution().isDigital("+100"));
         System.out.println(new Solution().isDigital("5E2"));
         System.out.println(new Solution().isDigital("5e2"));
+        System.out.println(new Solution().isDigital("-E-16"));
         System.out.println(new Solution().isDigital("-123"));
         System.out.println(new Solution().isDigital("3.14"));
-        System.out.println(new Solution().isDigital("-E-16"));
+        System.out.println(new Solution().isDigital(".14"));
+        System.out.println(new Solution().isDigital("3."));
+        System.out.println();
         System.out.println(new Solution().isDigital("12e"));
         System.out.println(new Solution().isDigital("1a3.14"));
         System.out.println(new Solution().isDigital("1.2.3"));
