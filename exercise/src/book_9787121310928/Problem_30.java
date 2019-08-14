ebbe52e37a3761ed8bdee2e653b1377726d28138
Apollo4634 <apollo4634@gmail.com>
2019-08-15 01:27:17 +0800

    0815-0127 剑指offer 30

---
+++ exercise/src/book_9787121310928/Problem_30.java
@@ -0,0 +1,38 @@
+package book_9787121310928;
+
+/**
+ * 包含min函数的栈
+ * 定义栈的数据结构，实现在O(1)复杂度内调用min、push、pop函数
+ *
+ * @author Apollo4634
+ * @create 2019/08/14
+ */
+
+public class Problem_30 {
+
+    static class Solution {
+        static class MyStack<E> {
+            int N;
+            Object[] arr;
+
+            MyStack(int capacity) {
+                this.N = capacity;
+                this.arr = new Object[N];
+            }
+
+            E min() {
+
+                return null;
+            }
+
+            void push(E e) {
+
+            }
+
+            E pop() {
+
+                return null;
+            }
+        }
+    }
+}
