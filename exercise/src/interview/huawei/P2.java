9fa3fccf64eb03d53323a7ec9556fca69021cf8e
Apollo4634 <apollo4634@gmail.com>
2019-08-31 23:05:19 +0800

    0831-2305 huawei

---
+++ exercise/src/interview/huawei/P2.java
@@ -0,0 +1,27 @@
+package interview.huawei;
+
+import java.util.Scanner;
+
+/**
+ * a,,1,"b,"""
+ * @author Apollo4634
+ * @create 2019/08/31
+ */
+
+public class P2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != '"') {
+                //int idx = str.indexOf();
+            }
+        }
+
+
+
+    }
+}
---
+++ exercise/src/interview/huawei/P3.java
@@ -0,0 +1,18 @@
+package interview.huawei;
+
+import java.util.Scanner;
+
+/**
+ * @author Apollo4634
+ * @create 2019/08/31
+ */
+
+public class P3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+}
