6f7eee7eeb208eb0bbce76b09d18a3d5eee3d2bc
Apollo4634 <apollo4634@gmail.com>
2019-09-01 02:38:28 +0800

    0901-0238 华为笔试题

--- exercise/src/interview/huawei/P1.java
+++ exercise/src/interview/huawei/P1.java
@@ -2,9 +2,11 @@ package interview.huawei;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 /**
- * int整数的二进制流中101出现的次数和首次出现的位置
+ * int整数的二进制流中101出现的次数和首次出现的位置。
+ * 位置从又开始计数，最右边的字符位置是0。
  *
  * @author Apollo4634
  * @create 2019/08/31
@@ -13,46 +15,23 @@ import java.util.Queue;
 public class P1 {
 
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int num = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
-        System.out.println(0x80000000);
+        String str = Integer.toBinaryString(num);
+        str = new StringBuilder(str).reverse().toString();
 
-        int num = 0;
-        if (num == Integer.MIN_VALUE) {
-            System.out.println(0+" "+-1); return;
-        } else if (num == Integer.MAX_VALUE) {
-            System.out.println(0+" "+-1); return;
-        }
-
-        //String str = Integer.toBinaryString(num);
-
-        for (int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; i++) {
-            String str = Integer.toBinaryString(num);
-
-            int flag = 1;
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < 32; j++) {
-                sb.append(flag&num);
-                flag = flag << 1;
+        int count = 0;
+        int firstIndex = -1;
+        for (int i = 0; i < str.length()-2; i++) {
+            char ch1 = str.charAt(i);
+            char ch2 = str.charAt(i+1);
+            char ch3 = str.charAt(i+2);
+            if (ch1=='1' && ch2=='0' && ch3=='1') {
+                if (count == 0) firstIndex = i;
+                count += 1;
             }
-            System.out.println(sb.toString());
         }
-
-
-
-
-//        int count = 0;
-//        int firstIndex = -1;
-//        for (int i = 0; i < str.length()-2; i++) {
-//            char ch1 = str.charAt(i);
-//            char ch2 = str.charAt(i+1);
-//            char ch3 = str.charAt(i+2);
-//            if (ch1=='1' && ch2=='0' && ch3=='1') {
-//                if (count == 0) firstIndex = i;
-//                count += 1;
-//            }
-//        }
-//        System.out.println(count+" "+firstIndex);
+        System.out.println(count+" "+firstIndex);
     }
 }
