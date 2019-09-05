fc80bc0c455cbeaa31d194ff2715c4cf4f01eee0
Apollo4634 <apollo4634@gmail.com>
2019-09-06 01:07:06 +0800

    0906-0106 剑指offer 50

---
+++ exercise/src/book_9787121310928/Problem_50.java
@@ -0,0 +1,23 @@
+package book_9787121310928;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 第一个只出现一次的字符
+ * @author Apollo4634
+ * @create 2019/09/06
+ */
+
+public class Problem_50 {
+
+    static class Solution {
+        char findFirstNotRepeatChar(String str) {
+            if (str == null || str.length() == 0) return '\0';
+            Set<Character> set = new HashSet<>(str.length());
+
+
+            return '\0';
+        }
+    }
+}
