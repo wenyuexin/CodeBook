fbc6f8f4f1e7e419082b991b0bde772fac9f03b3
Apollo4634 <apollo4634@gmail.com>
2019-09-24 14:26:43 +0800

    0924-1426

--- exercise/src/interview/baidu/SwapTwoNumsWithoutTempVariable.java
+++ exercise/src/interview/baidu/SwapTwoNumsWithoutTempVariable.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 /**
  * 在不使用辅助变量的情况下交换两个数字
- * 
+ *
+ * 为了避免溢出的问题，这里对同号和异号两种情况做了不同处理
+ *
  * @author Apollo4634
  * @create 2019/09/24
  */
@@ -14,11 +16,11 @@ public class SwapTwoNumsWithoutTempVariable {
     private static int[] swap(int a, int b) {
         if (a == b) return new int[] { a,b };
 
-        if (a>0 ^ b>0) {
+        if (a>0 ^ b>0) { //异号
             a = a + b;
             b = a - b;
             a = a - b;
-        } else {
+        } else { //同号
             a = a - b;
             b = a + b;
             a = b - a;
